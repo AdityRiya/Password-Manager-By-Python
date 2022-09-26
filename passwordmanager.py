68bec6a69265e0f1241a2fb67c386684df791e94
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-26 21:18:26 +0600

    update

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,12 +1,18 @@
 pwd = input("What is the master password? ")
 
+def view():
+    pass
+def add():
+    pass
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones (view or add)? ").lower()
-
-    if mode == "view":
+    mode = input("Would you like to add a new password or view existing ones (view or add) or press 'q' to quit? ").lower()
+    if mode == "q":
+        break
+    elif mode == "view":
         pass
     elif mode == "add":
         pass
     else:
         print("Invalid Mode.")
+        continue
